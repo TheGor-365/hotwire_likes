7e961d2287ae36dfa1cb31b26c56b10b349ba2a6
Gor <7495500@gmail.com>
2024-10-28 21:52:50 +0300

    Add minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar, :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :avatar, :name ])
   end
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,68 +2,51 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy like unlike ]
   before_action :authenticate_user!, except: %i[ index show ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
+  def show; end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy!
 
     respond_to do |format|
       format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   def like
     current_user.likes.create(likeable: @post)
     render partial: "posts/post", locals: { post: @post }
-    # restpod_to do |format|
-    #   format.turbo_stream
-    # end
   end
 
   def unlike
@@ -72,12 +55,11 @@ class PostsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:title, :content)
   end
